7c80ff5947c1bea11fc0b9e73e7c0069170dd9f2
Jordan Stephens <iam@jordanstephens.net>
2012-01-02 13:29:27 -0600

    color maintains HSL values from RGB

--- lib/paleta/color.rb
+++ lib/paleta/color.rb
@@ -1,7 +1,7 @@
 module Paleta
   class Color
     
-    attr_reader :red, :green, :blue
+    attr_reader :red, :green, :blue, :hue, :saturation, :lightness
     
     def initialize(red = 0, green = 0, blue = 0)
       self.red = red
@@ -10,21 +10,63 @@ module Paleta
     end
     
     def red=(val)
-      @red = range_validator(val)
+      @red = range_validator(val, 0..255)
+      update_hsl
     end
     
     def green=(val)
-      @green = range_validator(val)
+      @green = range_validator(val, 0..255)
+      update_hsl
     end
     
     def blue=(val)
-      @blue = range_validator(val)
+      @blue = range_validator(val, 0..255)
+      update_hsl
+    end
+    
+    def lightness=(val)
+      @lightness = range_validator(val, 0..100)
+    end
+    
+    def saturation=(val)
+      @saturation = range_validator(val, 0..100)
+    end
+    
+    def hue=(val)
+      @hue = range_validator(val, 0..360)
     end
     
     private
     
-    def range_validator(val)
-      val >= 0 && val <= 255 ? val : raise(ArgumentError, "Component range exceeded")
+    def update_hsl
+      r = @red / 255.0 rescue 0
+      g = @green / 255.0 rescue 0
+      b = @blue / 255.0 rescue 0
+      
+      min = [r, g, b].min
+      max = [r, g, b].max
+      delta = max - min
+      
+      @hue = 0
+      @saturation = 0
+      @lightness = (max + min) / 2.0
+      
+      if delta != 0
+        @saturation = (@lightness < 0.5) ? delta / (max + min) : delta / (2.0 - max - min)
+        case max
+        when r; @hue = (g - b) / delta
+        when g; @hue = 2 + (b - r) / delta
+        when b; @hue = 4 + (r - g) / delta
+        end
+      end
+      @hue *= 60
+      @hue += 360 if @hue < 0
+      @saturation *= 100
+      @lightness *= 100
+    end
+    
+    def range_validator(val, range)
+      range.include?(val) ? val : raise(ArgumentError, "Component range exceeded")
     end
   end
 end
--- spec/models/color_spec.rb
+++ spec/models/color_spec.rb
@@ -2,13 +2,38 @@ require 'spec_helper'
 
 describe Paleta::Color do
   it "should initialize with components in 0..255" do
-    color = Paleta::Color.new(22, 33, 44)
-    color.red.should == 22
-    color.green.should == 33
-    color.blue.should == 44
+    color = Paleta::Color.new(94, 161, 235)
+    color.red.should == 94
+    color.green.should == 161
+    color.blue.should == 235
   end
   
   it "should not initialize with components not in 0..255" do
     expect{ Paleta::Color.new(-74, 333, 4321) }.to raise_error
+  end
+  
+  it "should calculate its HSL value on itialization" do
+    color = Paleta::Color.new(237, 172, 33)
+    color.hue.to_i.should == 40
+    color.saturation.to_i.should == 85
+    color.lightness.to_i.should == 52
+  end
+  
+  it "should update its HSL value when its RGB values are updated" do
+    color = Paleta::Color.new(237, 172, 33)
+    color.red = 0
+    color.hue.to_i.should == 131
+    color.saturation.to_i.should == 100
+    color.lightness.to_i.should == 33
+    
+    color.green = 123
+    color.hue.to_i.should == 136
+    color.saturation.to_i.should == 100
+    color.lightness.to_i.should == 24
+
+    color.blue = 241
+    color.hue.to_i.should == 209
+    color.saturation.to_i.should == 100
+    color.lightness.to_i.should == 47
   end
 end
