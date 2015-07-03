338e66ce922b2e822770f2adb593beacd92c0f3b
Jordan Stephens <iam@jordanstephens.net>
2015-07-02 22:44:15 -0700

    alert missing rmagick on method call, rather than on load

--- lib/paleta.rb
+++ lib/paleta.rb
@@ -3,4 +3,13 @@ require 'paleta/color'
 require 'paleta/palette'
 
 module Paleta
+  @rmagick_available = begin
+    require "rmagick"
+  rescue LoadError
+    false
+  end
+
+  def self.rmagick_available?
+    @rmagick_available
+  end
 end
